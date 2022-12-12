e70321a70239c3cdcf94632eededb6a6723d624a
Dasha <dnazarkina@ya.ru>
2022-12-12 19:19:55 +0300

    added details for Ex 56

--- HT_Dec_Ex56/Program.cs
+++ HT_Dec_Ex56/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int rows = ReadInt("Pls enter qty of rows: ");
+int columns = ReadInt("Pls enter qty of columns: ");
+int[,] numbers = new int[rows, columns];
+
+FillMatrixRandomNumbers(numbers);
+WriteMatrix(numbers);
+
+{
+    int index = 0, minsum = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            sum += numbers[i, j];
+        }
+        Console.WriteLine($"the sum {i + 1} of raws = {sum}");
+        if (i == 0)
+        {
+            minsum = sum;
+        }
+        else if (sum < minsum)
+        {
+            minsum = sum;
+            index = i;
+        }
+    }
+    string line = string.Empty;
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        line += numbers[index, j] + " ";
+    }
+    Console.WriteLine($"The row with min sum {line}. ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillMatrixRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void WriteMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
