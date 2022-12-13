281d9f6c1409edf5a70f383ab5be9bf31c44fdfa
Dasha <dnazarkina@ya.ru>
2022-12-13 17:42:37 +0300

    added changes in Ex 64

--- HT_Dec_Ex64/Program.cs
+++ HT_Dec_Ex64/Program.cs
@@ -1,12 +1,10 @@
-﻿// /*Задача 64: Задайте значение N. Напишите программу, 
-// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
-Console.WriteLine("Pls enter natural number: ");
+Console.WriteLine("Pls enter natural number:");
 int number = int.Parse(Console.ReadLine());
 
 void NumberCounter (int number)
 {
-    // if (number < 0) Console.Write($"{number} is not natural");
     if (number == 0) return;
     Console.Write("{0,4}", number);
     NumberCounter (number - 1);
