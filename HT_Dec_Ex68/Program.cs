d26be572b2f84a0cb80381f6ca969c18ba48c603
Dasha <dnazarkina@ya.ru>
2022-12-13 17:54:57 +0300

    details added to Ex68

--- HT_Dec_Ex68/Program.cs
+++ HT_Dec_Ex68/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+Console.Write("Pls enter first number M = ");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.Write("Pls enter second number N = ");
+int numberN = int.Parse(Console.ReadLine());
+
+///Метод вычисления функции Аккермана:
+int AckermannFunction (int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return AckermannFunction(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
+return AckermannFunction(numberM, numberN);
+}
+
+Console.WriteLine($"Accerman's function: A({numberM},{numberN}) = {AckermannFunction(numberM, numberN)}");
